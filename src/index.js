8f9393e45a7a48ad4db6d97eae869364d8d20d30
Darina33 <115955389+Darina33@users.noreply.github.com>
2023-04-22 16:15:31 +0300

    s

---
+++ src/index.js
@@ -0,0 +1,94 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import LoadMoreBtn from './components/ReadMoreButton.js';
+import './sass/index.scss';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import pixabayApiService from "./pixabayApiService.js";
+
+const refs = {
+    form: document.getElementById("search-form"),
+    gallery: document.querySelector(".gallery"),
+}
+
+const pixabay = new pixabayApiService();
+const loadMoreBtn = new LoadMoreBtn({
+    selector: ".load-more",
+    isHidden: true,
+})
+const gallery = new SimpleLightbox('.gallery a');
+
+
+refs.form.addEventListener("submit", onSubmit);
+loadMoreBtn.button.addEventListener("click", fetchImage)
+
+
+function onSubmit(e) {
+    e.preventDefault();
+    loadMoreBtn.show();
+    const form = e.currentTarget;
+    pixabay.query = form.elements.searchQuery.value;
+
+    clearNewsList();
+    pixabay.resetPage();
+    fetchImage().finally(() => form.reset())
+}
+
+async function fetchImage() {
+    loadMoreBtn.disable();
+    
+
+    try {
+        const markup = await getImagesMarkup();
+        updateNewsList(markup);
+        loadMoreBtn.enable();
+    } catch (err) {
+        onerror(err)
+    }
+}
+
+async function getImagesMarkup() {
+    try {
+        const { hits } = await pixabay.getImage();
+        console.log(hits);
+        if (hits.length === 0)
+            throw new Error("Sorry, there are no images matching your search query. Please try again.");
+            return hits.reduce((markup, hits) => markup + createMarkup(hits), "")
+        } catch (err) {
+            onError(err);
+        }
+}
+    
+function createMarkup({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) {
+    return `<div class="photo-card"><a href="${largeImageURL}" alt="${tags}" class="gallery__link">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy class="gallery__image" /></a>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes ${likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views ${views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments ${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads ${downloads}</b>
+    </p>
+  </div>
+</div>`
+}
+
+function updateNewsList(markup) {
+    if (markup !== undefined)
+        refs.gallery.insertAdjacentHTML("beforeend", markup)
+}
+
+function clearNewsList() {
+    refs.gallery.innerHTML = '';
+}
+
+function onError(err) {
+    console.error(err);
+    loadMoreBtn.hide();
+    clearNewsList();
+}
---
+++ src/pixabayApiService.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+
+export default class pixabayApiService {
+    static ENDPOINT = "https://pixabay.com/api/";
+    static API_KEY = "35592946-b30e38cecc5f402f2c111ab69";
+
+    constructor() {
+    this.query = "";
+    this.page = 1;
+    }
+    
+    async getImage() {
+      const url = `${pixabayApiService.ENDPOINT}?key=${pixabayApiService.API_KEY}&q=${this.query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=4&page=${this.page}`;
+
+      const { data } = await axios.get(url);
+      this.incrementPage();
+
+      return data;
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+}
